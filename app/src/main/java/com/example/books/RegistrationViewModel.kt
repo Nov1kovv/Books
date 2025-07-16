d8055345f1471843f0539ec71c1626fe7e0220e1
Oleg Novikov <novikov7@yahoo.com>
2025-07-16 18:35:26 +0300

    save point

--- app/src/main/java/com/example/books/RegistrationViewModel.kt
+++ app/src/main/java/com/example/books/RegistrationViewModel.kt
@@ -6,17 +6,17 @@ import androidx.lifecycle.ViewModel
 
 class RegistrationViewModel : ViewModel() {
 
-    private val _login = mutableStateOf("")
-    val login: State<String> = _login
+    private val _login = mutableStateOf("") //mutableStateOf позволяет Compose отслеживать изменения и перерисовывать UI при изменении значения.
+    val login: State<String> = _login //неизменяемое состояние для доступа из UI
 
     private val _password = mutableStateOf("")
     val password: State<String> = _password
 
-    fun onLoginChange(newLogin: String) {
-        _login.value = newLogin
+    fun onLoginChange(newLogin: String) { // Метод, который вызывается из UI при изменении текста логина.
+        _login.value = newLogin // Обновляет значение login, что вызывает перерисовку UI.
     }
 
-    fun onPasswordChange(newPassword: String) {
+    fun onPasswordChange(newPassword: String) { // Метод, который вызывается из UI при изменении текста пароля.
         _password.value = newPassword
     }
 }
---
+++ app/src/main/java/com/example/books/domain/model/Book.kt
@@ -0,0 +1,4 @@
+package com.example.books.domain.model
+
+class Book {
+}

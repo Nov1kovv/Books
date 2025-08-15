0114f22dfc79f6b1307b66326b63207376580dc7
Oleg Novikov <novikov7@yahoo.com>
2025-08-15 16:28:53 +0300

    Login screen

--- app/src/main/java/com/example/books/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/books/ui/signup/SignUpScreen.kt
@@ -1,16 +1,21 @@
 package com.example.books.ui.signup
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.books.ui.registration.RegistrationViewModel
 import com.example.books.ui.registration.mvi.RegistrationAction
@@ -36,19 +41,46 @@ fun SignUpScreen(navController: NavController, viewModel: RegistrationViewModel)
         }
     }
 
+    val backgroundColor = Color(0xFF121212)
+    val cardColor = Color(0xFF1E1E1E)
+    val textPrimary = Color.White
+    val textSecondary = Color(0xFFB0B0B0)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(backgroundColor)
             .systemBarsPadding()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
+
+        Text(
+            "Регистрация",
+            color = textPrimary,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
 
         TextField(
             value = state.login,
             onValueChange = { viewModel.dispatch(RegistrationAction.UpdateLogin(it)) },
-            label = { Text("Логин") },
+            label = { Text("Логин",color = textSecondary) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = cardColor,
+                unfocusedContainerColor = cardColor,
+                focusedTextColor = textPrimary,
+                unfocusedTextColor = textPrimary,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
             modifier = Modifier.fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -56,17 +88,28 @@ fun SignUpScreen(navController: NavController, viewModel: RegistrationViewModel)
         TextField(
             value = state.password,
             onValueChange = { viewModel.dispatch(RegistrationAction.UpdatePassword(it)) },
-            label = { Text("Пароль") },
+            label = { Text("Пароль",color = textSecondary) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = cardColor,
+                unfocusedContainerColor = cardColor,
+                focusedTextColor = textPrimary,
+                unfocusedTextColor = textPrimary,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
             modifier = Modifier.fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         Button(
             onClick = {
                 viewModel.dispatch(RegistrationAction.SubmitRegister)
             },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth()
+            .height(50.dp),
+            shape = RoundedCornerShape(12.dp),
             enabled = !state.isLoading
         ) {
             Text(if (state.isLoading) "Загрузка..." else "Зарегистрироваться")
@@ -84,13 +127,12 @@ fun SignUpScreen(navController: NavController, viewModel: RegistrationViewModel)
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = "Уже есть аккаунт?")
-            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "Уже есть аккаунт?", color = textSecondary)
+            Spacer(modifier = Modifier.width(6.dp))
             Text(
                 text = "Войти",
                 color = MaterialTheme.colorScheme.primary,
                 fontWeight = FontWeight.Bold,
-                textDecoration = TextDecoration.Underline,
                 modifier = Modifier.clickable {
                     navController.navigate("login")
                 }

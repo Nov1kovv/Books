225543dbfd23985af5b75bfa0834149cba28494b
Oleg Novikov <novikov7@yahoo.com>
2025-08-22 07:28:17 +0300

    Save point

--- app/src/main/java/com/example/books/di/AppModule.kt
+++ app/src/main/java/com/example/books/di/AppModule.kt
@@ -6,16 +6,30 @@ import com.example.domain.repository.BookRepository
 import com.example.books.ui.bottombar.profile.ProfileViewModel
 import com.example.books.ui.catalog.BookCatalogViewModel
 import com.example.books.ui.registration.RegistrationViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
+    
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
 
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
     single {
         Retrofit.Builder()
             .baseUrl("https://www.googleapis.com/books/v1/")
+            .client(get())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         //логирование запрос inreceptor
